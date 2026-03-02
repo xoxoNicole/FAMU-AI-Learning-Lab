9d7df8b2c90301f0aaf4a1226d978220f3aca451
Nicole Murray <delishtable@gmail.com>
2026-03-02 22:22:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1627840436-58ff1",
+  "appId": "1:129473286856:web:629357aadb7e0684ea4bfd",
+  "apiKey": "AIzaSyD6Ek5bhch3o-CVPfv5ArZITx0kLo2GjAg",
+  "authDomain": "studio-1627840436-58ff1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "129473286856"
+};
